6bfe7de39c16c44a6a876d95a4ee502a9c7f1ccb
Sungwon Um <shineum@gmail.com>
2024-05-27 21:30:29 -0700

    update admin - inline items

--- apps/northwind/admin.py
+++ apps/northwind/admin.py
@@ -3,9 +3,52 @@ from . import models
 
 # Register your models here.
 
+class __AdminReadOnlyTabularInline(admin.TabularInline):
+    extra = 0
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+        
+
+class TerritoriesInline(__AdminReadOnlyTabularInline):
+    model = models.Territories
+
+
+class EmployeesInline(__AdminReadOnlyTabularInline):
+    model = models.Employees    
+    fields = ('employee_id', '__custom_name__', 'title')
+    readonly_fields = ('__custom_name__', )
+
+    def __custom_name__(self, obj):
+        return obj.__str__()
+    __custom_name__.short_description = "name"
+
+
+class ProductsInline(__AdminReadOnlyTabularInline):
+    model = models.Products
+    fields = ('product_id', 'product_name', 'unit_price', 'units_on_order')
+
+
+class OrdersInline(__AdminReadOnlyTabularInline):
+    model = models.Orders
+    fields = ('order_id', 'customer', 'order_date')
+
+
+class OrderDetailsInline(__AdminReadOnlyTabularInline):
+    model = models.OrderDetails
+    fields = ('order_id', 'product_id', 'quantity')
+
+
 @admin.register(models.Region)
 class RegionAdmin(admin.ModelAdmin):
     list_display = ["region_id", "region_description"]
+    inlines = [TerritoriesInline]
 
 
 @admin.register(models.Territories)
@@ -16,36 +59,43 @@ class TerritoriesAdmin(admin.ModelAdmin):
 @admin.register(models.Employees)
 class EmployeesAdmin(admin.ModelAdmin):
     list_display = ["employee_id", "__str__", "title", "reports_to"]
+    inlines = [EmployeesInline, OrdersInline]
 
 
 @admin.register(models.Shippers)
 class ShippersAdmin(admin.ModelAdmin):
     list_display = ["company_name", "phone"]
+    inlines = [OrdersInline]
 
 
 @admin.register(models.Customers)
 class CustomersAdmin(admin.ModelAdmin):
     list_display = ["customer_id", "company_name", "country", "contact_title", "contact_name"]
+    inlines = [OrdersInline]
 
 
 @admin.register(models.Categories)
 class CategoriesAdmin(admin.ModelAdmin):
     list_display = ["category_id", "category_name", "description"]
+    inlines = [ProductsInline]
 
 
 @admin.register(models.Suppliers)
 class SuppliersAdmin(admin.ModelAdmin):
     list_display = ["supplier_id", "company_name", "country", "contact_title", "contact_name"]
+    inlines = [ProductsInline]
 
 
 @admin.register(models.Products)
 class ProductsAdmin(admin.ModelAdmin):
     list_display = ["product_id", "product_name", "supplier_id", "category_id", "units_in_stock", "discontinued"]
+    inlines = [OrderDetailsInline]
 
 
 @admin.register(models.Orders)
 class OrdersAdmin(admin.ModelAdmin):
     list_display = ["order_id", "customer", "employee", "order_date", "shipped_date", "ship_via", "ship_country"]
+    inlines = [OrderDetailsInline]
 
 
 @admin.register(models.USStates)
