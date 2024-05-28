0183ccc600956a51c2e354220a3921650915ed24
Sungwon Um <shineum@gmail.com>
2024-05-27 20:25:06 -0700

    set basic admin

--- apps/northwind/admin.py
+++ apps/northwind/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Region)
+class RegionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Territories)
+class TerritoriesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Employees)
+class EmployeesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Shippers)
+class ShippersAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.CustomerDemographics)
+class CustomerDemographicsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Customers)
+class CustomersAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Suppliers)
+class SuppliersAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Products)
+class ProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.OrderDetails)
+class OrderDetailsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.USStates)
+class USStatesAdmin(admin.ModelAdmin):
+    pass
