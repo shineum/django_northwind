f6f2901e965581a927e694c2e7943dfdc100d96c
Sungwon Um <shineum@gmail.com>
2024-05-28 00:15:23 -0700

    update urls for yasg (swagger)

--- apps/northwind/urls.py
+++ apps/northwind/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from . import views
 
 router = routers.DefaultRouter()
@@ -15,6 +17,17 @@ router.register(r'products', views.ProductsViewSet)
 router.register(r'orders', views.OrdersViewSet)
 router.register(r'usstates', views.USStatesViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Northwind API", 
+        default_version='v1', 
+    ),
+    public=True,
+)
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),    
 ]
